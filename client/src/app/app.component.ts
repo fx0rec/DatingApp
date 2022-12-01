7cead6aefc3a91890aef5b1c1232e45e1ebae9fb
John Doe <opposition21@protonmail.com>
2022-12-01 03:30:38 +0100

    Section 5.11: Parent to Child commmunication

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,3 @@
-import { HttpClient } from '@angular/common/http';
-import { ReturnStatement } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
 import { User } from './_models/user';
 import { AccountService } from './_services/account.service';
@@ -12,9 +10,9 @@ import { AccountService } from './_services/account.service';
 export class AppComponent implements OnInit{
   //properties
   title: string = 'DatingApp';
-  users: any;
+
   //constructor
-  constructor(private http: HttpClient, private accountService: AccountService) // Injecting HttpClient & AccountService
+  constructor(private accountService: AccountService) // Injecting HttpClient & AccountService
   {
 
   }
@@ -22,22 +20,10 @@ export class AppComponent implements OnInit{
   //On initialization we make a request to our API server through Http, 
   //which is injected into our class through the constructor
   ngOnInit(): void {
-   this.getUsers(); //gets Users on init
    this.setCurrentUser(); //sets current User on init, if something is in local storage
   }
 
-  getUsers(){
-     //@return — An Observable of the response, with the response body as an ArrayBuffer.
-    //An observable is a stream of data, that we wish to observe in some way as it's returned from the API. 
-    //Observables are lazy by nature and won't happen unless subscribed to them using the .subscribe() method.
-    //We specify what happens next:, followed by a call-back function describing what we wan't to do with the returned data.
-    //We''re gonna get a response back from Get(), and the lambda => specifies what do to with it. 
-    this.http.get('https://localhost:5001/api/users').subscribe({
-      next: response => this.users = response, 
-      error: error => {console.log(error)},
-      complete: () => console.log('Request has completed!')
-    });
-  }
+  
 
   setCurrentUser(){
     const userString = localStorage.getItem('user');
--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,14 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
   registerMode = false;
+  users: any; //To work with the getUsers()
 
-  constructor() { }
+  constructor(private http: HttpClient) { } //Injecting HttpClient to work with getUsers()
 
   ngOnInit(): void {
+    this.getUsers();
   }
 
   registerToggle(){
     this.registerMode = !this.registerMode; //sets registerMode to the opposite of whatever it is currently
   }
+
+  getUsers(){
+    //@return — An Observable of the response, with the response body as an ArrayBuffer.
+   //An observable is a stream of data, that we wish to observe in some way as it's returned from the API. 
+   //Observables are lazy by nature and won't happen unless subscribed to them using the .subscribe() method.
+   //We specify what happens next:, followed by a call-back function describing what we wan't to do with the returned data.
+   //We''re gonna get a response back from Get(), and the lambda => specifies what do to with it. 
+   this.http.get('https://localhost:5001/api/users').subscribe({
+     next: response => this.users = response, 
+     error: error => {console.log(error)},
+     complete: () => console.log('Request has completed!')
+   });
+ }
 
 }
