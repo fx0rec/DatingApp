af2e966dcd9682c67fcab910741542ed2256d6d0
John Doe <opposition21@protonmail.com>
2022-12-06 07:36:00 +0100

    8: Creating a repository

---
+++ API/Data/UserRepository.cs
@@ -0,0 +1,44 @@
+
+using API.Entities;
+using API.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly DataContext _context;
+        public UserRepository(DataContext context)
+        {
+            _context = context;
+            
+        }
+        public async Task<AppUser> GetUserByIdAsync(int id)
+        {
+            return await _context.Users.FindAsync(id);
+        }
+
+        public async Task<AppUser> GetUserByUsernameAsync(string username)
+        {
+            return await _context.Users.FirstOrDefaultAsync(x => x.UserName == username); // x=> x.UserName == username clarifies that AppUser.UserName == username 
+        }
+
+        public async Task<IEnumerable<AppUser>> GetUsersAsync()
+        {
+            return await _context.Users.ToListAsync(); //Returns a list of all users
+        }
+
+        public async Task<bool> SaveAllAsync()
+        {
+            return await _context.SaveChangesAsync() > 0; //Save changes if changes are greater than 0
+        }
+
+        public void Update(AppUser user)
+        {
+            _context.Entry(user).State = EntityState.Modified;
+            //This tells our Entity framework tracker that an entity has been updated.
+            //Changes aren't saved in this stage. 
+            //The need of this is debated, as Entity will know the changes. But it's included just in case
+        }
+    }
+}
