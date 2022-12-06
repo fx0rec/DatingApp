63f8f10cc4e300ab5c49347e429a7dcbe31dc5c1
John Doe <opposition21@protonmail.com>
2022-12-06 14:42:00 +0100

    8: Configuring AutoMapper

--- API/Helpers/AutoMapperProfiles.cs
+++ API/Helpers/AutoMapperProfiles.cs
@@ -9,10 +9,18 @@ namespace API.Helpers
         //The idea of AutoMapper, is that it takes away the writing of tedious code.
         public AutoMapperProfiles() 
         {
-            CreateMap<AppUser, MemberDto>(); //Maps AppUser to MemberDto
+            CreateMap<AppUser, MemberDto>()
+            .ForMember(dest => dest.PhotoUrl, opt => opt.
+            MapFrom(src => src.Photos.FirstOrDefault(x=>x.IsMain).Url)); 
+            //Gets the Photo Url from AppUser.Photo and sets it to PhotoUrl string in MemberDto. 
+
+
             CreateMap<Photo, PhotoDto>();   //Maps Photo to PhotoDto
             //It simply maps the properties from one to another, and is smart enough to infer that the int Age = GetAge()
             //It needs to be injected into the controller, so it needs a service.
+
+
+
         }
     }
 }
