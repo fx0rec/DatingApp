8d23bf37cf45503f06b39be9c61e0884876c1340
John Doe <opposition21@protonmail.com>
2022-12-08 15:17:34 +0100

    9: Adding styling to the cards

--- client/src/app/members/member-card/member-card.component.ts
+++ client/src/app/members/member-card/member-card.component.ts
@@ -1,10 +1,12 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
 import { Member } from 'src/app/_models/member';
 
 @Component({
   selector: 'app-member-card',
   templateUrl: './member-card.component.html',
   styleUrls: ['./member-card.component.css']
+  //encapsulation: ViewEncapsulation.Emulated
+
 })
 export class MemberCardComponent implements OnInit {
 @Input() member: Member | undefined; //member is currently undefined, we don't have access to it yet. This way we get around the TS error
