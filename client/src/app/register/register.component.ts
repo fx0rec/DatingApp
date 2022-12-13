3f264eed4f84c6e61ab3fba5c6ae870a19104a4c
John Doe <opposition21@protonmail.com>
2022-12-13 20:18:46 +0100

    Section 12: Reactive forms intro & validation

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, ValidatorFn, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { AccountService } from '../_services/account.service';
 
@@ -11,23 +13,37 @@ export class RegisterComponent implements OnInit {
   //@Input() userFromHomeComponent: any; //Parent to child (Input)
   @Output() cancelRegister = new EventEmitter(); //Child to parent (Output)
   model: any = {};
+  registerForm: FormGroup = new FormGroup({});
 
-  constructor(private accountService: AccountService, private toastr: ToastrService) { }
+  constructor(private accountService: AccountService, private toastr: ToastrService, private fb: FormBuilder, private router: Router) { }
 
   ngOnInit(): void {
+    this.initializeForm();
   }
 
-  register(){
-    this.accountService.register(this.model).subscribe({
-      next: () => {
-        this.toastr.success('Registered!')
-        this.cancel();
-      },
-      error: error => {this.toastr.error(error.error);
-      console.log(error);
-      }
-
+  initializeForm(){
+    this.registerForm = new FormGroup({
+      username: new FormControl('', Validators.required),
+      password: new FormControl('', [Validators.required, Validators.minLength(4), Validators.maxLength(8)]),
+      confirmPassword: new FormControl('', Validators.required)
     })
+  }
+
+
+
+  register(){
+    // this.accountService.register(this.model).subscribe({
+    //   next: () => {
+    //     this.toastr.success('Registered!')
+    //     this.cancel();
+    //   },
+    //   error: error => {this.toastr.error(error.error);
+    //   console.log(error);
+    //   }
+
+    // })
+
+    console.log(this.registerForm?.value);
   }
 
   cancel(){
